7245fc0412ab89badd28e852fb250f22cadad138
unknown <wooofah@gmail.com>
2020-02-21 17:10:28 +0200

    react-native-app

---
+++ App.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, FlatList } from 'react-native';
+import { Navbar } from './src/Navbar'
+import { AddTodo } from './src/AddTodo';
+import { Todo } from './src/Todo';
+
+export default function App() {
+
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (title) => {
+    setTodos(prevTodos => [
+      ...prevTodos,
+      {
+        id: Date.now().toString(),
+        title
+      }
+    ])
+  }
+
+  const removeTodo = (id) => {
+    setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id))
+  }
+
+  return (
+    <View>
+      <Navbar title="Todo App" />
+      <View style={styles.container}>
+        <AddTodo onSubmit={addTodo} />
+        <FlatList
+          keyExtractor={item => item.id}
+          data={todos}
+          renderItem={({ item }) => (<Todo todo={item} onRemove={removeTodo}/>)}
+
+        />
+      </View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 30,
+    paddingVertical: 20
+  }
+});
